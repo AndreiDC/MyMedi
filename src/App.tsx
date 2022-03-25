6a04136406edef81c2dbc9378dcf68bc38eb9e2c
a-zelenschi <a.zelenschi@gmail.com>
2022-03-25 03:02:34 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Icon } from "@iconify/react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Icon icon="ant-design:close-outlined" />;
+}
+
+export default App;
---
+++ src/components/tailwindcss-button/TailwindcssButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { TailwindcssButton } from "./TailwindcssButton";
+export default {
+  title: "components/tailwindcss-button/TailwindcssButton",
+};
+
+export const Button = () => {
+  return <TailwindcssButton />;
+};
