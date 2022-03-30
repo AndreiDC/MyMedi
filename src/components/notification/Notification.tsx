df1c85812b3b0093ca3a238627cce82aafa3f0bc
a-zelenschi <a.zelenschi@gmail.com>
2022-03-30 23:21:00 +0300

    notification

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -3,10 +3,10 @@ import { IconClose } from "./IconClose";
 
 export const Notification = () => {
   return (
-    <div className="w-100% pr-4 pl-4 mr-auto ml-auto box-content ">
-      <div className="text-xs pt-2 pr-9 pb-2 pl-0 text-center text-blue-900 font-sans ">
+    <div className="w-100% pr-4 pl-4 pt-1 mr-auto ml-auto box-content  ">
+      <div className="text-sm  pr-9 pb-2  text-center text-blue-900 font-sans font-medium">
         Due to the
-        <strong> COVID 19 </strong>
+        <strong className="text-base"> COVID 19 </strong>
         epidemic, orders may be processed with a slight delay
       </div>
       <IconClose />
