379a5fbd5335306061b472413b9f91973f69c4c5
a-zelenschi <a.zelenschi@gmail.com>
2022-03-30 23:16:28 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { Notification } from "./components/notification/Notification";
+import { TopMenu } from "./components/top-menu/TopMenu";
+import { MainMenu } from "./components/main-menu/MainMenu";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="container mx-auto ">
       <Notification />
+      <div className="hidden md:contents">
+        <TopMenu />
+      </div>
+
+      <div className="bg-[url('https://dummyimage.com/1920x886/cccccc/969696.jpg')]">
+        <MainMenu />
+      </div>
     </div>
   );
 }
