49311e9f2082cfb68ef834221cb3a8396f5ec7f2
a-zelenschi <a.zelenschi@gmail.com>
2022-03-31 21:24:38 +0300

    create languageCurrency component

---
+++ src/components/main-menu/LanguageCurrency.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+export const LanguageCurrency = () => {
+  return (
+    <div className=" flex items-center space-x-2  font-sans text-blue-900 ">
+      <div className="flex items-end  ">
+        English
+        <Icon className="w-3 h-3 ml-1 " icon="bi:chevron-down" />
+      </div>
+
+      <div className="flex items-end  ">
+        Shop
+        <Icon className="w-3 h-3 ml-1 " icon="bi:chevron-down" />
+      </div>
+    </div>
+  );
+};
