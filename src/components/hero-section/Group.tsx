929d3a2936361998e8f8bfba38bebf2edc00fed7
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 22:58:53 +0300

    create Group component

---
+++ src/components/hero-section/Group.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+export const Group = () => {
+  return (
+    <div className="flex justify-center text-orange-500">
+      <div className="text-lg flex align-middle flex-col font-medium mb-4 ">
+        <div className="flex mb-4 ">
+          <div className="text-2xl">
+            <Icon icon="mdi:bacteria-outline" />
+          </div>
+          <div>Anti-Bacterial</div>
+        </div>
+        <div className="flex">
+          <div className="text-2xl">
+            <Icon icon="arcticons:coronavirus" />
+          </div>
+          <div>Anti-Virus</div>
+        </div>
+      </div>
+    </div>
+  );
+};
