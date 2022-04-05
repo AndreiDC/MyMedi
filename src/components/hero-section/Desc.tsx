1fd41138a582f6ef9f75fb88ab4ed067490880ac
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 23:01:14 +0300

    create Desc

---
+++ src/components/hero-section/Desc.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Desc = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="text-blue-900 flex align-middle">
+        Now with special price -30%
+      </div>
+    </div>
+  );
+};
