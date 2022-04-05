7b432fd70305c90d2678d43622071e8812f860bc
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 22:58:02 +0300

    add mb-9

--- src/components/hero-section/Desc.tsx
+++ src/components/hero-section/Desc.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Desc = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center mb-9">
       <div className="text-blue-900 flex align-middle">
         Now with special price -30%
       </div>
