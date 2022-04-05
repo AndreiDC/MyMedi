6e8aebb643bc0a2d644f665338663849287f4681
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 23:00:08 +0300

    chande style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Notification } from "./components/notification/Notification";
 import { TopMenu } from "./components/top-menu/TopMenu";
 import { MainMenu } from "./components/main-menu/MainMenu";
+import { HeroSection } from "./components/hero-section/HeroSection";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,9 +13,13 @@ function App() {
       <div className="hidden md:contents">
         <TopMenu />
       </div>
-
-      <div className="bg-[url('https://dummyimage.com/1920x886/cccccc/969696.jpg')]">
-        <MainMenu />
+      <div className=" min-h-screen bg-cover bg-center bg-[url('https://dummyimage.com/1920x886/cccccc/969696.jpg')]">
+        <div className="border-b border-white">
+          <MainMenu />
+        </div>
+        <div className="pt-8">
+          <HeroSection />
+        </div>
       </div>
     </div>
   );
