b1f291086277a88315ce5e9a81f506efc8d2f01f
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 23:02:06 +0300

    create Title

---
+++ src/components/hero-section/Title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const Title = () => {
+  return (
+    <div className=" flex justify-center mb-6">
+      <div className=" text-blue-900 flex align-middle text-4xl text-center font-semibold ">
+        SonicX900
+        <br />
+        Electrical Brush
+      </div>
+    </div>
+  );
+};
