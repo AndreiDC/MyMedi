78253071da7c26630c50fa6180ad0c6f1ce6b572
a-zelenschi <a.zelenschi@gmail.com>
2022-04-05 23:00:41 +0300

    create baged

---
+++ src/components/hero-section/Baged.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Baged = () => {
+  return (
+    <div className="w-100% h-100 mt-10 mb-6 ">
+      <div className=" m-auto w-12 h-5 text-xs bg-green-600 text-white border-green-600 rounded-full  flex justify-center   ">
+        <div className="flex items-center  sans-serif"> New</div>
+      </div>
+    </div>
+  );
+};
