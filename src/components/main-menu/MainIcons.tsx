3f7496cb30d2fb9d8afa9cfae01776e45a428d1f
a-zelenschi <a.zelenschi@gmail.com>
2022-04-07 19:46:53 +0300

    add lg mr-7

--- src/components/main-menu/MainIcons.tsx
+++ src/components/main-menu/MainIcons.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 
 export const MainIcons = () => {
   return (
-    <div className="flex items-center  text-orange-400 space-x-6">
+    <div className="flex items-center  text-orange-400 space-x-6 lg:mr-7">
       <Icon className="h-6 w-6" icon="bi:search" />
       <Icon className="h-6 w-6" icon="bi:person-circle" />
       <Icon className="h-6 w-6" icon="bi:suit-heart" />
