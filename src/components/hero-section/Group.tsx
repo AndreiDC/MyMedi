ba102e52a18aae4e5735f5987e50089d5fbc561b
a-zelenschi <a.zelenschi@gmail.com>
2022-04-09 08:33:32 +0300

    change atrigute in grup, shop

--- src/components/hero-section/Group.tsx
+++ src/components/hero-section/Group.tsx
@@ -3,8 +3,8 @@ import { Icon } from "@iconify/react";
 
 export const Group = () => {
   return (
-    <div className="flex justify-center text-orange-500 font-serif lg:hidden ">
-      <div className="text-lg flex align-middle flex-col font-medium mb-4   ">
+    <div className="flex justify-center text-orange-500 font-serif  ">
+      <div className="text-lg flex align-middle  flex-col font-medium mb-4 lg:flex-row lg:space-x-10  ">
         <div className="flex mb-4  ">
           <div className="text-2xl">
             <Icon icon="mdi:bacteria-outline" />
--- src/components/hero-section/Shop.tsx
+++ src/components/hero-section/Shop.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Shop = () => {
   return (
-    <div className="mt-4 flex justify-center lg:hidden ">
+    <div className="mt-4 flex justify-center  ">
       <div className=" inline-block align-middle">
         <button className="bg-orange-500 rounded-full text-white px-7 py-2 text-xl font-medium">
           Shop now
